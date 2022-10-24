8fc3e40435803120386036ff8f9dc5ed71e78b78
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2022-10-24 10:22:50 -0400

    Add fields and read combine stub

--- src/models/config.rs
+++ src/models/config.rs
@@ -1,23 +1,38 @@
+use std::path::{PathBuf};
+
 use color_eyre::Result;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, Hash, PartialEq, Eq)]
 #[allow(non_snake_case)]
 #[serde(rename_all = "camelCase")]
 pub struct UserConfig {
-
+    pub cache: PathBuf,
+    pub timeout: u32,
 }
 
 impl UserConfig {
     fn read() -> Result<Self> {
-
         Ok(UserConfig {
-
+            cache: todo!(),
+            timeout: todo!(),
         })
     }
 
     fn write(&self) -> Result<()> {
-
         Ok(())
+    }
+
+    pub fn read_combine() -> Self {
+        todo!()
+    }
+}
+
+impl Default for UserConfig {
+    fn default() -> Self {
+        Self {
+            cache: Default::default(),
+            timeout: Default::default(),
+        }
     }
 }
