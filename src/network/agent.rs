feb5ca05ab0165d12d45fb02d8b9e6a3084e0bda
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2022-10-31 22:34:54 -0400

    Use timeout from config

--- src/network/agent.rs
+++ src/network/agent.rs
@@ -1,11 +1,6 @@
-use std::{
-    sync,
-    time::Duration,
-};
+use std::{sync, time::Duration};
 
-
-// TODO: config
-const TIMEOUT: u64 = 5000;
+use crate::models::config::UserConfig;
 
 static AGENT: sync::OnceLock<reqwest::blocking::Client> = sync::OnceLock::new();
 
@@ -13,8 +8,9 @@ pub fn get_agent() -> &'static reqwest::blocking::Client {
     AGENT.get_or_init(|| {
         reqwest::blocking::ClientBuilder::new()
             .connect_timeout(Duration::from_millis(
-                TIMEOUT,
+                UserConfig::read_combine().unwrap().timeout.unwrap().into(),
             ))
+            .tcp_nodelay(true)
             .user_agent(format!("questpackagemanager-rust2/{}", env!("CARGO_PKG_VERSION")).as_str())
             .build()
             .expect("Client agent was not buildable")
