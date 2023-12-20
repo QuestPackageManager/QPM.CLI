410d027bcf0db357c28a8c27130c1c3ec0d9ca71
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-12-20 15:32:28 -0400

    Enable test

--- src/tests/network/qpackages.rs
+++ src/tests/network/qpackages.rs
@@ -45,8 +45,6 @@ fn get_artifact() -> Result<()> {
 
 #[test]
 fn resolve() -> Result<()> {
-    // TODO: Fix
-    return Ok(());
     let repo = QPMRepository::default();
     let version = Version::new(0, 33, 0);
     let p = repo.get_package("codegen", &version)?;
