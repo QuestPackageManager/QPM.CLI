5880619014caf559fcf89ffb86c8e88537ea231a
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2024-04-08 21:15:31 -0400

    Add `qpm qmod build` for generating qmod zips

---
+++ src/commands/qmod/build.rs
@@ -0,0 +1,139 @@
+use std::fs::{read_to_string, File};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
+use clap::Args;
+use itertools::Itertools;
+
+use qpm_qmod::models::mod_json::ModJson;
+
+use crate::commands::qmod::manifest::{generate_qmod_manifest, ManifestQmodOperationArgs};
+use crate::models::mod_json::ModJsonExtensions;
+use crate::models::package::PackageConfigExtensions;
+use crate::terminal::colors::QPMColor;
+
+use qpm_package::models::dependency::SharedPackageConfig;
+
+use qpm_package::models::package::PackageConfig;
+
+use color_eyre::eyre::ensure;
+
+use color_eyre::Result;
+
+#[derive(Args, Debug, Clone)]
+pub struct BuildQmodOperationArgs {
+    ///
+    /// Tells QPM to exclude mods from being listed as copied mod or libs dependencies
+    ///
+    #[clap(long = "exclude_libs")]
+    pub exclude_libs: Option<Vec<String>>,
+
+    ///
+    /// Tells QPM to include mods from being listed as copied mod or libs dependencies
+    /// Does not work with `exclude_libs` combined
+    ///
+    #[clap(long = "include_libs")]
+    pub include_libs: Option<Vec<String>>,
+
+    ///
+    /// Adds directories for qpm to look for files. Not recursive
+    ///
+    #[clap(long = "include_libs")]
+    pub include_dirs: Option<Vec<String>>,
+
+    #[clap(long, default_value = "false")]
+    pub(crate) offline: bool,
+
+    #[clap()]
+    pub(crate) out_target: PathBuf,
+}
+
+// This will parse the `qmod.template.json` and process it, then finally export a `qmod.json` for packaging and deploying.
+pub(crate) fn execute_qmod_build_operation(build_parameters: BuildQmodOperationArgs) -> Result<()> {
+    ensure!(std::path::Path::new("mod.template.json").exists(),
+        "No mod.template.json found in the current directory, set it up please :) Hint: use \"qmod create\"");
+
+    println!("Generating mod.json file from template using qpm.shared.json...");
+    let package = PackageConfig::read(".")?;
+    let shared_package = SharedPackageConfig::read(".")?;
+
+    let new_manifest = generate_qmod_manifest(
+        &package,
+        shared_package,
+        ManifestQmodOperationArgs {
+            exclude_libs: build_parameters.exclude_libs.clone(),
+            include_libs: build_parameters.include_libs.clone(),
+            offline: build_parameters.offline,
+        },
+    )?;
+
+    let look_for_files = |s: &str| {
+        build_parameters
+            .include_dirs
+            .as_ref()
+            .unwrap_or_else(|| panic!("No include dirs provided for looking for files!"))
+            .iter()
+            .find(|path| Path::new(path).join(s).exists())
+            .map(|path| PathBuf::from(path.clone()))
+            .unwrap_or_else(|| {
+                panic!(
+                    "No file found for {s} in directories {}",
+                    build_parameters.include_dirs.as_ref().unwrap().join(";")
+                )
+            })
+    };
+
+    let file_copies_list = new_manifest
+        .file_copies
+        .iter()
+        .map(|c| look_for_files(&c.name));
+    let late_mod_list = new_manifest
+        .late_mod_files
+        .iter()
+        .map(|c| look_for_files(c));
+    let early_mod_list = new_manifest.mod_files.iter().map(|c| look_for_files(c));
+    let lib_list = new_manifest.library_files.iter().map(|c| look_for_files(c));
+
+    let combined_files = file_copies_list
+        .chain(late_mod_list)
+        .chain(early_mod_list)
+        .chain(lib_list)
+        .collect_vec();
+
+    let out_target_qmod = build_parameters.out_target.with_extension("qmod");
+    
+    println!(
+        "Writing qmod zip {}",
+        out_target_qmod.to_string_lossy().file_path_color()
+    );
+    println!(
+        "Using files: {}",
+        combined_files
+            .iter()
+            .map(|s| format!("\t{}", s.to_string_lossy().file_path_color()))
+            .join("\n")
+    );
+    let mut zip_file = File::create(out_target_qmod)?;
+
+    let mut zip = zip::ZipWriter::new(&mut zip_file);
+
+    let options =
+        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
+    for file in combined_files {
+        println!("Adding file {}", file.to_string_lossy().file_path_color());
+        // 50kb
+        let contents = String::with_capacity(1024 * 50);
+        read_to_string(&file)?;
+
+        zip.start_file(file.file_name().unwrap().to_string_lossy(), options)?;
+        zip.write_all(contents.as_bytes())?;
+    }
+
+    zip.start_file(ModJson::get_result_name(), options)?;
+    serde_json::to_writer_pretty(&mut zip, &new_manifest)?;
+    // Apply the changes you've made.
+    // Dropping the `ZipWriter` will have the same effect, but may silently fail
+    zip.finish()?;
+
+    Ok(())
+}
--- src/commands/qmod/mod.rs
+++ src/commands/qmod/mod.rs
@@ -1,8 +1,9 @@
 use clap::{Args, Subcommand};
-use color_eyre::{owo_colors::OwoColorize, Result};
+use color_eyre::{Result};
 
 use super::Command;
 
+mod build;
 mod create;
 mod edit;
 mod manifest;
@@ -22,9 +23,9 @@ pub enum QmodOperation {
     /// Some properties are not settable through the qmod create command, these properties are either editable through the package, or not at all
     Create(create::CreateQmodJsonOperationArgs),
     /// This will parse the `mod.template.json` and process it, then finally export a `mod.json` for packaging and deploying.
-    Manifest(manifest::BuildQmodOperationArgs),
+    Manifest(manifest::ManifestQmodOperationArgs),
     /// Deprecated alias for manifest
-    Build(manifest::BuildQmodOperationArgs),
+    Build(build::BuildQmodOperationArgs),
     /// Edit your mod.template.json from the command line, mostly intended for edits on github actions
     ///
     /// Some properties are not editable through the qmod edit command, these properties are either editable through the package, or not at all
@@ -35,15 +36,8 @@ impl Command for QmodCommand {
     fn execute(self) -> Result<()> {
         match self.op {
             QmodOperation::Create(q) => create::execute_qmod_create_operation(q),
-            QmodOperation::Build(b) => {
-                println!(
-                    "{} is deprecated, switch to {}",
-                    "qpm qmod build".yellow(),
-                    "qpm qmod manifest".green()
-                );
-                manifest::execute_qmod_build_operation(b)
-            }
-            QmodOperation::Manifest(b) => manifest::execute_qmod_build_operation(b),
+            QmodOperation::Build(b) => build::execute_qmod_build_operation(b),
+            QmodOperation::Manifest(b) => manifest::execute_qmod_manifest_operation(b),
             QmodOperation::Edit(e) => e.execute(),
         }
     }
