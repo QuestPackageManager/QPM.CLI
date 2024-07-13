cf0f900563a55044b8f726f8eb2071665920812f
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-07-13 18:01:25 -0400

    Reorganize, filter `use` to installed only by default and add download

--- src/commands/ndk.rs
+++ src/commands/ndk.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, path::Path};
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+};
 
 use clap::{Args, Subcommand};
 use color_eyre::{
@@ -63,11 +66,14 @@ pub struct UseArgs {
     #[clap(long, default_value = "false")]
     strict: bool,
 
-    #[clap(long, default_value = "false")]
+    #[clap(long, default_value = "true")]
     installed_only: bool,
 }
 #[derive(Args, Debug, Clone)]
-pub struct ResolveArgs {}
+pub struct ResolveArgs {
+    #[clap(short, long, default_value = "false")]
+    download: bool,
+}
 
 fn fuzzy_match_ndk<'a>(
     manifest: &'a AndroidRepositoryManifest,
@@ -167,97 +173,122 @@ impl Command for Ndk {
 
                 println!("{}", ndk_path.display());
             }
-            NdkOperation::Resolve(r) => {
-                let package = PackageConfig::read(".")?;
-                let ndk_requirement = package.workspace.ndk.as_ref();
-
-                let Some(ndk_requirement) = ndk_requirement else {
-                    bail!("No NDK requirement set in project")
-                };
-                // find latest NDK that satisfies requirement
-                let ndk_installed_path = get_combine_config()
-                    .get_ndk_installed()
-                    .into_iter()
-                    .flatten()
-                    .sorted_by(|a, b| a.file_name().cmp(b.file_name()))
-                    .rev() // descending
-                    .find(|s| {
-                        Version::parse(s.file_name().to_str().unwrap())
-                            .is_ok_and(|version| ndk_requirement.matches(&version))
-                    })
-                    .ok_or_else(|| {
-                        let report =
-                            eyre!("No NDK version installed that satisfies {ndk_requirement}");
-
-                        // look up a version suitable to work with
-                        // allow this to work offline by handling safely
-                        let manifest = get_android_manifest().ok();
-                        let suggested_version = manifest
-                            .as_ref()
-                            .and_then(|manifest| range_match_ndk(manifest, ndk_requirement).ok());
-
-                        match suggested_version {
-                            Some((suggested_version, _)) => {
-                                report.suggestion(format!("qpm ndk download {suggested_version}"))
-                            }
-                            None => report,
-                        }
-                    })?;
-
-                apply_ndk(ndk_installed_path.path())?;
-            }
-            NdkOperation::Use(u) => {
-                let version = match u.installed_only {
-                    true => {
-                        let version_req = VersionReq::parse(&u.version)?;
-                        // find latest NDK that satisfies requirement
-                        let ndk_installed_path = get_combine_config()
-                            .get_ndk_installed()
-                            .into_iter()
-                            .flatten()
-                            .sorted_by(|a, b| a.file_name().cmp(b.file_name()))
-                            .rev()
-                            .find(|n| {
-                                Version::parse(n.file_name().to_str().unwrap())
-                                    .is_ok_and(|v| version_req.matches(&v))
-                            })
-                            .ok_or_else(|| {
-                                eyre!("No NDK version instaleled that satisfies {version_req}")
-                            })?;
-
-                        ndk_installed_path.file_name().to_str().unwrap().to_string()
-                    }
-                    // allow any NDK version online
-                    false => {
-                        let manifest = get_android_manifest()?;
-                        fuzzy_match_ndk(&manifest, &u.version)?.0
-                    }
-                };
-
-                let mut package = PackageConfig::read(".")?;
-                let req = match u.strict {
-                    true => format!("={version}"),
-                    false => format!("^{version}"),
-                };
-                package.workspace.ndk = Some(VersionReq::parse(&req)?);
-                package.write(".")?;
-
-                let ndk_path = format!(
-                    "{}/{version}",
-                    get_combine_config()
-                        .ndk_download_path
-                        .as_ref()
-                        .unwrap()
-                        .to_str()
-                        .unwrap()
-                );
-
-                apply_ndk(Path::new(&ndk_path))?;
-            }
+            NdkOperation::Resolve(r) => do_resolve(r)?,
+            NdkOperation::Use(u) => do_use(u)?,
         }
 
         Ok(())
     }
+}
+
+fn do_use(u: UseArgs) -> Result<(), color_eyre::eyre::Error> {
+    let version = match u.installed_only {
+        true => {
+            let version_req = VersionReq::parse(&u.version)?;
+            // find latest NDK that satisfies requirement
+            let ndk_installed_path = get_combine_config()
+                .get_ndk_installed()
+                .into_iter()
+                .flatten()
+                .sorted_by(|a, b| a.file_name().cmp(b.file_name()))
+                .rev()
+                .find(|n| {
+                    Version::parse(n.file_name().to_str().unwrap())
+                        .is_ok_and(|v| version_req.matches(&v))
+                })
+                .ok_or_else(|| {
+                    eyre!("No NDK version installed that satisfies {version_req}")
+                })?;
+
+            ndk_installed_path.file_name().to_str().unwrap().to_string()
+        }
+        // allow any NDK version online
+        false => {
+            let manifest = get_android_manifest()?;
+            fuzzy_match_ndk(&manifest, &u.version)?.0
+        }
+    };
+    let mut package = PackageConfig::read(".")?;
+    let req = match u.strict {
+        true => format!("={version}"),
+        false => format!("^{version}"),
+    };
+    package.workspace.ndk = Some(VersionReq::parse(&req)?);
+    package.write(".")?;
+    let ndk_path = format!(
+        "{}/{version}",
+        get_combine_config()
+            .ndk_download_path
+            .as_ref()
+            .unwrap()
+            .to_str()
+            .unwrap()
+    );
+    apply_ndk(Path::new(&ndk_path))?;
+    Ok(())
+}
+
+fn do_resolve(r: ResolveArgs) -> Result<(), color_eyre::eyre::Error> {
+    let package = PackageConfig::read(".")?;
+    let ndk_requirement = package.workspace.ndk.as_ref();
+    let Some(ndk_requirement) = ndk_requirement else {
+        bail!("No NDK requirement set in project")
+    };
+    let ndk_installed_path_opt = get_combine_config()
+        .get_ndk_installed()
+        .into_iter()
+        .flatten()
+        .sorted_by(|a, b| a.file_name().cmp(b.file_name()))
+        .rev() // descending
+        .find(|s| {
+            Version::parse(s.file_name().to_str().unwrap())
+                .is_ok_and(|version| ndk_requirement.matches(&version))
+        });
+    let ndk_installed_path: PathBuf = match ndk_installed_path_opt {
+        // NDK Found, unwrap
+        Some(ndk_installed_path) => ndk_installed_path.path().into(),
+        // download
+        None if r.download => {
+            let manifest = get_android_manifest()?;
+            let (version, _package) = range_match_ndk(&manifest, ndk_requirement)?;
+
+            let ndk_path = format!(
+                "{}/{version}",
+                get_combine_config()
+                    .ndk_download_path
+                    .as_ref()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+            );
+
+            ndk_path.into()
+        }
+        // error
+        _ => {
+            let mut report =
+                eyre!("No NDK version installed that satisfies {ndk_requirement}")
+                    .note("-d/--download not set, not downloading!");
+
+            // look up a version suitable to work with
+            // allow this to work offline by handling safely
+            let manifest = get_android_manifest().ok();
+            let mut suggested_version = manifest
+                .as_ref()
+                .and_then(|manifest| range_match_ndk(manifest, ndk_requirement).ok());
+
+            if let Some((suggested_version, _)) = &mut suggested_version {
+                report =
+                    report.suggestion(format!("qpm ndk download {suggested_version}"));
+            }
+
+            return Err(report);
+        }
+    };
+
+    // apply the NDK to the environment
+    apply_ndk(&ndk_installed_path)?;
+    Ok(())
 }
 
 // apply NDK to project and write
