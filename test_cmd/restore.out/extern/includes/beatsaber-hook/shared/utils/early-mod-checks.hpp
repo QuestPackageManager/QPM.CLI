dd061c6c6e789127ce883d710346bf48c1799023
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-08-05 23:37:42 -0400

    Make test case smaller

--- test_cmd/restore.out/extern/includes/beatsaber-hook/shared/utils/early-mod-checks.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Include this file to assert that you have defined the necessary functions with the correct signatures
-
-#include <type_traits>
-#include "scotland2/shared/loader.hpp"
-
-static_assert(std::is_same_v<modloader::SetupFunc, decltype(&setup)>, "Must match the specified signature with setup!");
-static_assert(std::is_same_v<modloader::LoadFunc, decltype(&load)>, "Must match the specified signature with load!");
--- test_cmd/restore.out/extern/includes/beatsaber-hook/shared/utils/enum-type.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "enum-wrapper-type.hpp"
-
-namespace bs_hook {
-    #pragma pack(push, et, 0)
-    struct EnumType {};
-    #pragma pack(pop, et)
-}
