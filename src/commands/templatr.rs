6b5f1ca87abf9331bf97d15de29449ca6469cead
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-09-02 12:58:38 -0400

    Fix conflicting short arg

--- src/commands/templatr.rs
+++ src/commands/templatr.rs
@@ -12,7 +12,7 @@ pub struct TemplatrCommand {
     /// Destination where template will be copied to. FILES WILL BE OVERWRITTEN
     dest: String,
 
-    #[clap(short, long)]
+    #[clap(short = 'b', long)]
     git_branch: Option<String>,
 }
 
