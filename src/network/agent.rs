11f0312b16e67470b849df8481743c9ed4c3708c
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-09-02 20:45:58 -0400

    Replace tokio async download with sync. Much faster and stable

--- src/network/agent.rs
+++ src/network/agent.rs
@@ -1,7 +1,13 @@
-use std::{env, io::Write, sync, time::Duration};
+use std::{
+    env,
+    io::{ErrorKind, Read, Write},
+    sync,
+    thread::sleep,
+    time::Duration,
+};
 
 use color_eyre::{
-    eyre::{bail, Context},
+    eyre::{bail, ensure, eyre, Context},
     Result,
 };
 use pbr::ProgressBar;
@@ -10,8 +16,6 @@ use reqwest::StatusCode;
 use crate::models::config::get_combine_config;
 
 static AGENT: sync::OnceLock<reqwest::blocking::Client> = sync::OnceLock::new();
-static ASYNC_AGENT: sync::OnceLock<reqwest::Client> = sync::OnceLock::new();
-static RUNTIME: sync::OnceLock<tokio::runtime::Runtime> = sync::OnceLock::new();
 
 pub fn get_agent() -> &'static reqwest::blocking::Client {
     let timeout = get_combine_config().timeout.unwrap_or(5000);
@@ -28,61 +32,52 @@ pub fn get_agent() -> &'static reqwest::blocking::Client {
     })
 }
 
-pub fn get_async_agent() -> &'static reqwest::Client {
-    let timeout = get_combine_config().timeout.unwrap_or(5000);
-
-    ASYNC_AGENT.get_or_init(|| {
-        reqwest::ClientBuilder::new()
-            .connect_timeout(Duration::from_millis(timeout.into()))
-            .read_timeout(Duration::from_millis(timeout.into()))
-            .tcp_keepalive(Duration::from_secs(5))
-            .tcp_nodelay(false)
-            .https_only(true)
-            .user_agent(format!("questpackagemanager-rust2/{}", env!("CARGO_PKG_VERSION")).as_str())
-            .build()
-            .expect("Client agent was not buildable")
-    })
-}
-
-pub fn get_runtime() -> &'static tokio::runtime::Runtime {
-    RUNTIME.get_or_init(|| {
-        tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .expect("Unable to build tokio runtime")
-    })
-}
-
 pub fn download_file<F>(url: &str, buffer: &mut impl Write, mut callback: F) -> Result<usize>
 where
     F: FnMut(usize, usize),
 {
-    let mut request = get_async_agent().get(url).build()?;
+    let mut request = get_agent().get(url).build()?;
 
     request.timeout_mut().take(); // Set to none
 
-    get_runtime().block_on(async {
-        let mut response = get_async_agent()
-            .execute(request)
-            .await
-            .with_context(|| format!("Unable to download file {url}"))?
-            .error_for_status()?;
-        if response.status() == StatusCode::NOT_FOUND {
-            bail!("Not found!");
+    let mut response = get_agent()
+        .execute(request)
+        .with_context(|| format!("Unable to download file {url}"))?
+        .error_for_status()?;
+
+    let expected_amount = response.content_length().unwrap_or(0) as usize;
+    let mut written: usize = 0;
+
+    let mut temp_buf = vec![0u8; 1024];
+
+    loop {
+        let read = response.read(&mut temp_buf);
+
+        match read {
+            // EOF
+            Ok(0) => break,
+            
+            Ok(amount) => {
+                written += amount;
+                buffer.write_all(&temp_buf[0..amount])?;
+                callback(written, expected_amount);
+            }
+            Err(e) if e.kind() == ErrorKind::Interrupted => {
+                sleep(Duration::from_millis(1));
+            }
+            Err(e) => {
+                return Err(e)
+                    .with_context(|| format!("Failed to continue reading bytes from {url}"))
+            }
         }
+    }
 
-        // TODO: Fix
-        let expected_amount = response.content_length().unwrap_or(0) as usize;
-        let mut written: usize = 0;
+    ensure!(
+        written == expected_amount,
+        "Read: 0x{written:x} Expected: 0x{expected_amount:x}"
+    );
 
-        while let Some(chunk) = response.chunk().await? {
-            written += chunk.len();
-            buffer.write_all(&chunk)?;
-            callback(written, expected_amount);
-        }
-
-        Ok(expected_amount)
-    })
+    Ok(expected_amount)
 }
 
 #[inline(always)]
