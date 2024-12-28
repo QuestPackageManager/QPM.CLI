d00e21d498ffdda08935313893ec38862af3b485
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-12-27 22:39:09 -0400

    Fix tests

--- src/tests/commands.rs
+++ src/tests/commands.rs
@@ -1,6 +1,16 @@
 #[test]
+#[cfg(feature = "cli")]
 fn trycmd() {
+    // #[cfg(test)]
+    // let bin = trycmd::cargo::cargo_bin!("qpm_cli");
+    // #[cfg(not(test))]
+    let bin = trycmd::cargo::cargo_bin("qpm");
+
+
+    assert!(bin.exists(), "Binary not found: {:?}", bin);
+
     trycmd::TestCases::new()
+        .default_bin_path(bin)
         .case("README.md")
         .pass("test_cmd/*.toml")
         .pass("test_cmd/*.trycmd")
--- tests_update.ps1
+++ tests_update.ps1
@@ -1,3 +1,4 @@
 $ENV:TRYCMD="overwrite"
+& cargo build --bin qpm 
 & cargo test --bin qpm -- tests::commands::trycmd -- --nocapture
 $ENV:TRYCMD=""
